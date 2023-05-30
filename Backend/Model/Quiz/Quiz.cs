c045e6518fb7d23c86c379dc7da867201b895e2c
anobie <anobie3tesfaye@gmail.com>
2023-05-30 11:19:27 +0300

    Quiz models

--- Backend/Model/Quiz/Quiz.cs
+++ Backend/Model/Quiz/Quiz.cs
@@ -1,22 +1,19 @@
 ﻿using Backend.Model.Book;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend.Model.Quiz
 {
     public class Quiz
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Question { get; set; }
-        public string CorrectAnswer { get; set; }
-        public string Explanation { get; set; }
-
-        public string Description { get; set; }
-
-        public string Department { get; set; }
-
+      
         public int Year { get; set; }
 
-        public string Username { get; set; }
+        public string Uploader { get; set; }
 
         public int Likes { get; set; } = 0;
 
@@ -24,10 +21,7 @@ namespace Backend.Model.Quiz
 
         public string MaterialType { get; set; }
 
-        public DateOnly Date { get; set; }
+        public string Date { get; set; }
 
-        public string? File { get; set; }
-
-        public Comment[] Comments { get; set; }
     }
 }
---
+++ Backend/Model/Quiz/QuizUploadModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Backend.Model.Quiz
+{
+    public class QuizUploadModel
+    {
+        public List<string> Question { get; set; }
+        public List<string> Choices { get; set; }
+    
+        public string CorrectAnswer { get; set; }
+        public string Explanation { get; set; }
+
+        public string Description { get; set; }
+
+        public string Duration { get; set; }
+    }
+}
