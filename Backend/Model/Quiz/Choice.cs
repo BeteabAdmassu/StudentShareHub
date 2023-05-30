73915c01a9bf313ad52adf30575659ea1aa55dd3
anobie <anobie3tesfaye@gmail.com>
2023-05-30 13:31:03 +0300

    quiz upload model

---
+++ Backend/Model/Quiz/Choice.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Model.Quiz
+{
+    public class Choice
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ChoiceId { get; set; }
+
+        public string choice { get; set; }
+        
+        public Question question { get; set; }  
+    }
+}
--- Backend/Model/Quiz/Quiz.cs
+++ Backend/Model/Quiz/Quiz.cs
@@ -1,15 +1,14 @@
-﻿using Backend.Model.Book;
+﻿
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend.Model.Quiz
 {
-    public class Quiz
+    public class Quiz:QuizUploadModel
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-        public string Title { get; set; }
+        public int QuizId { get; set; }
       
         public int Year { get; set; }
 
@@ -19,9 +18,10 @@ namespace Backend.Model.Quiz
 
         public int views { get; set; } = 0;
 
-        public string MaterialType { get; set; }
+        public string MaterialType { get; set; } = "Quiz";
 
-        public string Date { get; set; }
+        public string Date { get; set; } = DateTime.Now.ToString("MM/dd/yyyy");
+
 
     }
 }
